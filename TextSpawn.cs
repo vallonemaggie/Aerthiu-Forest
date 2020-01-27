4a8c9bea452508f0e35ddebeade2e6ddad480bd1
vallonemaggie <35276698+vallonemaggie@users.noreply.github.com>
2020-01-27 00:38:47 -0600

    Add files via upload
    I think these are all the relevant ones?

---
+++ TextSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TextSpawn : MonoBehaviour
+{
+    //[SerializeField]
+    public GameObject text;
+
+    void Start()
+    {
+        text.SetActive(false);
+    }
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            text.SetActive(true);
+            Destroy(text, 3.0f);
+            Destroy(this.gameObject, 3.0f);
+            Debug.Log("Text Spawned.");
+        }
+    }
+
+}
